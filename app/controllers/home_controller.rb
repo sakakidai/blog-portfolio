165655d38d10a6a83c87d3544841b1122a566bb3
sakaki dai <sd.lybd1016@gmail.com>
2019-06-21 17:08:39 +0900

    Date setting complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
+    @blogs = Blog.recent.page(params[:page]).per(15)
+    @comment = Comment.new
   end
 end
