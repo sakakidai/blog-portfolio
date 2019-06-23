d382e07e0e84b55ff9b06aba2b4f15dcef24351a
sakaki dai <sd.lybd1016@gmail.com>
2019-06-23 18:07:02 +0900

    Removed delete link and edit link from the view of the category and also removed the route

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: {minimum: 2, maximum: 20}
   validate :avatar_size
   validate :top_image_size
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_22_065612) do
+ActiveRecord::Schema.define(version: 2019_06_23_070437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 2019_06_22_065612) do
     t.string "avatar"
     t.string "top_image"
     t.text "color"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
